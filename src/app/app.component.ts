90189b51f410422a44fdfa0b1dfbbb815ea68401
marco <marcoantn020@gmail.com>
2025-01-15 16:37:47 -0300

    feat: create struct form and style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from "./components/header/header.component";
+import {FormsComponent} from "./components/forms/forms.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    HeaderComponent
+    HeaderComponent,
+    FormsComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
